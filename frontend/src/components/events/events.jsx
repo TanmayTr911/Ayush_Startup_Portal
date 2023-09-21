8fba7b4b77154a4dcf5a85ee7bc0bdddefe57f2d
TanmayTr911 <89075656+TanmayTr911@users.noreply.github.com>
2023-09-21 21:12:15 +0530

    final updates to events section

--- frontend/src/components/events/events.jsx
+++ frontend/src/components/events/events.jsx
@@ -13,40 +13,43 @@ const Events = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <div className="post" style={{color:"#009470",paddingBottom:"0px",fontSize:"3.5em",fontWeight:"bolder"}}><p>Upcoming Events</p></div>
       
-      <div className="post">
-        <div className="card" style={{ width: '40vw', marginRight:"2vw" ,borderRadius:"2vh" }}>
+      <div className="post" style={{paddingBottom:"0px"}}>
+        
+        
+        <div className="card" style={{ width: '30vw', marginRight:"4vw" ,borderRadius:"2vh" }}>
       <img className="card-img-top" src={yogadayimg} alt="Card image cap" />
       <div className="card-body">
         <h5 className="card-title">Yoga Day Celebration</h5>
         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" className="btn btn-primary" style={{background:"#009470"}}>I'm Interested</a>
+        <a href="#" className="btn btn-primary" style={{background:"#009470",border:"none",width:"100%"}}>I'm Interested</a>
       </div>
     </div>
-    <div className="card" style={{ width: '40vw', marginLeft:"2vw" ,borderRadius:"2vh" }}>
+    <div className="card" style={{ width: '30vw', marginLeft:"4vw" ,borderRadius:"2vh" }}>
       <img className="card-img-top" src={eventimg2}  alt="Card image cap" />
       <div className="card-body">
         <h5 className="card-title">Udaan 2021</h5>
         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" className="btn btn-primary" style={{background:"#009470"}}>I'm Interested</a>
+        <a href="#" className="btn btn-primary" style={{background:"#009470",border:"none",width:"100%"}}>I'm Interested</a>
       </div>
     </div>
     </div>
     <div className="post">
-    <div className="card" style={{ width: '40vw', marginRight:"2vw",borderRadius:"2vh" }}>
+    <div className="card" style={{ width: '30vw', marginRight:"4vw",borderRadius:"2vh" }}>
       <img className="card-img-top" src={eventimg3} alt="Card image cap" />
       <div className="card-body">
         <h5 className="card-title">2023 Calender Launch</h5>
         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" className="btn btn-primary" style={{background:"#009470"}}>I'm Interested</a>
+        <a href="#" className="btn btn-primary" style={{background:"#009470",border:"none",width:"100%"}}>I'm Interested</a>
       </div>
     </div>
-    <div className="card" style={{ width: '40vw', marginLeft:"2vw",borderRadius:"2vh" }}>
+    <div className="card" style={{ width: '30vw', marginLeft:"4vw",borderRadius:"2vh" }}>
       <img className="card-img-top" src={eventimg4} alt="Card image cap" />
       <div className="card-body">
         <h5 className="card-title">Ayurveda Event</h5>
         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" className="btn btn-primary" style={{background:"#009470"}}>I'm Interested</a>
+        <a href="#" className="btn btn-primary" style={{background:"#009470",border:"none",width:"100%"}}>I'm Interested</a>
       </div>
     </div>
        
